1d850201d901882ea51ce8a471332913c1f5e153
AndriiShnurkov <Shans12.84@ukr.net>
2021-07-20 09:43:38 +0300

    added products

--- src/data/data.js
+++ src/data/data.js
@@ -3,12 +3,13 @@ export const data = {
     { name: "Home", id: "/home" },
     { name: "About", id: "/about" },
     { name: "Contact us", id: "/contacts" },
-    { name: "Rеgister", id: "/register" },
+    { name: "Register", id: "/register" },
     { name: "Login", id: "/login" },
   ],
   products: {
     tools: [
       {
+        id: "rdhgh12331312",
         name: 'Набор инструментов Alloid 3/8" 78 предметов',
         image: "https://content.rozetka.com.ua/goods/images/big/11177915.jpg",
         description:
@@ -16,6 +17,7 @@ export const data = {
         price: 1399,
       },
       {
+        id: "dtryfjjg3g4jy2j3423",
         name: "Универсальный набор инструмента Dnipro-M ULTRA",
         image:
           "https://static.dnipro-m.ua/cache/products/2015/catalog_origin_184714.jpg",
@@ -24,13 +26,39 @@ export const data = {
         price: 2646,
       },
       {
-        name: 'Набор инструментов Alloid 3/8" 78 предметов',
+        id: "gjh43234h23jkh",
+        name: "Набор инструментов BAXTER",
         image: "https://content.rozetka.com.ua/goods/images/big/36292331.jpg",
         description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ducimus placeat libero asperiores possimus nisi consequuntur soluta, esse sint suscipit ut laboriosam. Officiis soluta amet quas repellendus doloremque nam labore, aut, dicta reprehenderit consequuntur impedit beatae a corrupti saepe, ipsam nisi aperiam totam dolore dolorem ullam id! Consequatur, itaque quasi?",
-        price: 1399,
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad eaque sunt saepe consequuntur nemo. Suscipit doloremque eius eos nam perspiciatis tempore, maxime asperiores doloribus quibusdam aperiam commodi iusto harum laborum quae? Repellat nostrum saepe perferendis?",
+        price: 2490,
       },
     ],
-    toys: [],
+    toys: [
+      {
+        id: "g3jhg4h23g4hg2hm4g234",
+        name: "Коляска для кукол Smoby Maxi Cosi 4 в 1",
+        image: "https://content.rozetka.com.ua/goods/images/big/10596310.jpg",
+        description:
+          "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil aliquid corrupti beatae doloribus quis exercitationem aliquam qui placeat, maiores, harum blanditiis recusandae. Adipisci ipsum laboriosam, libero nam assumenda facilis exercitationem unde quod deserunt magnam, quos nobis dolores atque.",
+        price: 1564,
+      },
+      {
+        id: "123h3kl12hk3jh1j23",
+        name: "Бластер Hasbro Nerf Elite Disruptor",
+        image: "https://content1.rozetka.com.ua/goods/images/big/10980329.jpg",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ducimus placeat libero asperiores possimus nisi consequuntur soluta, esse sint suscipit ut laboriosam. Officiis soluta amet quas repellendus doloremque nam labore, aut, dicta reprehenderit consequuntur impedit beatae a corrupti saepe, ipsam nisi aperiam totam dolore dolorem ullam id! Consequatur, itaque quasi?",
+        price: 390,
+      },
+      {
+        id: "h23kh4jh23j4h2",
+        name: "Машинка перевертыш на пульте радиоуправления",
+        image: "https://content2.rozetka.com.ua/goods/images/big/7717218.jpg",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ut et, dicta dolor incidunt at alias veniam, facilis animi ratione exercitationem quibusdam eligendi officia molestias nobis expedita totam, dolore necessitatibus? Natus quaerat rerum incidunt error, mollitia dolorem aut ratione.",
+        price: 1200,
+      },
+    ],
   },
 };
--- src/test/index.js
+++
@@ -1,5 +0,0 @@
-import friends from "./friends.json";
-import stats from "./statistics.json";
-
-const testData = { friends, stats };
-export default testData;
